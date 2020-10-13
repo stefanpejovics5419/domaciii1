f11074c29f0c6818e8a1796236a59c642e4dc476
stefanpejovics5419 <72813770+stefanpejovics5419@users.noreply.github.com>
2020-10-13 18:24:18 +0200

    Add files via upload

--- domaci1.php
+++ domaci1.php
@@ -1,3 +1,17 @@
-Стефан <br>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Domaci</title>
+
+ Стефан <br>
 Пејовић <br>
 s54/19
+
+
+</head>
+<body>
+    
+</body>
+</html>
